a7a125176924c0e826ec62c116bf4a487997007a
Memyllyss <efktop@gmail.com>
2026-04-01 07:52:28 -0300

    Add string concatenation function with message

---
+++ declaracao-variaveis/Concatenacao-de-String.js
@@ -0,0 +1,13 @@
+// concatenacao com nome e comida
+
+function concatenacao() {
+
+    const nome = "Estevão";
+    const comida = "sorvete";
+
+    return {
+        mensagem: `${nome} tem 16 anos e gosta de ${comida}`
+    };
+}
+
+export { concatenacao };
